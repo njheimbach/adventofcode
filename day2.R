10e602267f2cd1145b955732d142ab37466d39ae
njheimbach <74012701+njheimbach@users.noreply.github.com>
2021-12-02 18:34:43 +0100

    Add files via upload

---
+++ day2.R
@@ -0,0 +1,29 @@
+setwd("~/Desktop")
+dive <- read.table("dive.txt", header = FALSE, sep = "", dec = ".")
+
+forward = 0
+depth = 0
+
+for (row in 1:nrow(dive)) {
+  if(substr(dive$V1[row], 1,1) == "f") {
+    forward = forward + dive$V2[row]
+  }
+  if(substr(dive$V1[row], 1,1) == "d") {
+    depth = depth + dive$V2[row]
+  }
+  if(substr(dive$V1[row], 1,1) == "u") {
+    depth = depth - dive$V2[row]
+  }
+}
+
+result = forward * depth
+
+result
+
+
+f = sum(dive$V2[dive$V1 == "forward"])
+d = sum(dive$V2[dive$V1 == "down"])
+u = sum(dive$V2[dive$V1 == "up"])
+
+d-u
+
