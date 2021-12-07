db8b60224b03ff6dc0421fb60de3b23e493f9733
njheimbach <74012701+njheimbach@users.noreply.github.com>
2021-12-07 16:15:39 +0100

    Update day7.R
    add part two

--- day7.R
+++ day7.R
@@ -1,3 +1,5 @@
+# part 1 ----
+
 setwd("~/Desktop/advent_of_code/")
 fish <- read.table("day7.txt", header = FALSE, sep = ",", dec = ".")
 colnames(fish)<-NULL
@@ -24,5 +26,37 @@ coordinates$COST <- abs(coordinates$COST)
 result <- sum(coordinates$COST)
 result 
 
+rm(result)
+rm(fish)
+rm(coordinates)
+rm(best_coordinate)
 
 
+
+
+# part 2 ----
+setwd("~/Desktop/advent_of_code/")
+fish <- read.table("day7.txt", header = FALSE, sep = ",", dec = ".")
+colnames(fish)<-NULL
+rownames(fish)<-NULL
+
+# transpose so that you have one column with all the values
+coordinates <- data.frame(t(fish[1,]))
+
+#define a "result" which is very very high 
+result <- 10000000000
+
+
+for (i in 0:1985) {
+  a <- (ncol(coordinates)+1)
+  coordinates[,a] <- i
+  coordinates[,(a+1)] <- abs(coordinates[,a] - coordinates$X1)
+  coordinates[,(a+2)] <- (coordinates[,(a+1)] * (coordinates[,(a+1)]+1))/2
+  try <- sum(coordinates[,(a+2)])
+  if (try < result) {
+    result <- try
+    magic_number <- i
+  }
+}
+
+result
